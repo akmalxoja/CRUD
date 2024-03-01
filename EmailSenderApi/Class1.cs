8b049646a94d85bfad7e61a414da09230f19c28a
akmalxoja <akmalxojasharifxojayev@gmail.com>
2024-03-01 08:32:43 +0500

    asdasdasd

---
+++ EmailSenderApi/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSenderApi
+{
+    public class Class1
+    {
+    }
+}
